697c591cfdfaf9aba0444a656a2878332ea967da
M0simi <mish5508@gmail.com>
2025-02-16 11:28:41 +0000

    I can do it

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,20 +1,18 @@
-h/* 
-
-* File: 4.puts.c
-* Created by: Meshari Alosimi 10715@holbertonstudents.com
-*/
+/* 
+ * File: 4-puts.c
+ * Created by: Meshari Alosimi 10715@holbertonstudents.com
+ */
 
 #include <stdio.h>
 
 /**
-* main - print some text using puts to automaticly add new line 
-*
-*Return: 0 always
-*/
-
-
+ * main - prints some text using puts to automatically add a new line
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
