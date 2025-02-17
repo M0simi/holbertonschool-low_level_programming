6d2d45d007c85f7a6da3264c11cc84f2676a6d9b
M0siimi <mish5508@gmail.com>
2025-02-17 11:13:34 +0000

    my correction

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * description: print the lowercase alphabet except 'q' and 'e'
+ * Description: Prints the lowercase alphabet except 'q' and 'e'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+    char letter;
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			if (letter != 'q' && letter !='e') /* skip 'q' and 'e'*/
-				putchar(letter);
-		}
-	putchar('\n'); /*print newline at the end*/
-			 return (0);
-			 }
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e') /* Skip 'q' and 'e' */
+            putchar(letter);
+    }
+
+    putchar('\n'); /* Print newline at the end */
+
+    return (0);
+}
