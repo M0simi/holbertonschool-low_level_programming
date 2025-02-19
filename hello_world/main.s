4e86b4dc592169c1ec53d56b51bb4af6149a5f32
M0simi <mish5508@gmail.com>
2025-02-19 01:59:04 +0000

    advanced tasks

--- hello_world/main.s
+++ hello_world/main.s
@@ -1,5 +1,31 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello, World!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	call	puts@PLT
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
 	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
