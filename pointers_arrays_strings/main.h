035d6fbd3c55401216885487df7173960f1ff72f
M0simi <mish5508@gmail.com>
2025-03-05 03:38:17 +0000

    first task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *best, char *str);
 int _atoi(char *s);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif /*MAIN.H*/
