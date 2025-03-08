f888afbad9a08b6972d11a1ec26f157a0659c841
M0simi <mish5508@gmail.com>
2025-03-08 19:55:09 +0000

    next task

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse using recursion
+ * @s: Pointer to the string
+ *
+ * Return: void (nothing)
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0') /* Base case: Stop when reaching the end of the string */
+        return;
+
+    _print_rev_recursion(s + 1); /* Recursive call to move to the next character */
+    _putchar(*s); /* Print the character after the recursive call */
+}
