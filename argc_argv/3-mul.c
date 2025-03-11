230e8c6eda4c0902a76aa2805917fdc86558843f
M0simi <mish5508@gmail.com>
2025-03-11 07:02:51 +0000

    multibly

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers passed as arguments.
+ * @argc: Argument count (number of arguments passed).
+ * @argv: Argument vector (array of strings containing arguments).
+ *
+ * Return: 0 if successful, 1 if incorrect number of arguments is given.
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+
+/* Ensure exactly two arguments are provided */
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+/* Convert arguments from strings to integers */
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+
+/* Perform multiplication */
+result = num1 * num2;
+
+/* Print the result */
+printf("%d\n", result);
+
+return (0);
+}
