2555494b55bfe7911888f6f059715a8081723150
root <mish5508@gmail.com>
2025-03-17 06:26:07 +0000

    typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @name: Pointer to dog's name
  * @age: Dog's age
  * @owner: Pointer to owner's name
+ * define struct have the dog information
  */
 
 struct dog
@@ -14,6 +15,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/* deifne dog_t as an alternative name */
+typedef struct dog dog_t;
 
 /* function prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
