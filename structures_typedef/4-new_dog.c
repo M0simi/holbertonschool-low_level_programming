47e0ba5e60d3055284de0568760323c06f7d9f8a
M0simi <mish5508@gmail.com>
2025-03-17 20:02:36 +0000

    edit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,31 +12,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
 
+dog_t *new_dog;
+int name_len, owner_len;
 	/* memory allocation for the structure */
 	new_dog = malloc(sizeof(dog_t));
-
-
 	if (new_dog == NULL)
-
 	return (NULL);
 
+	/* calculate the length of name and owner */
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+
+
 	/* allocation and copy the name */
-	new_dog->name = strdup(name);
+	new_dog->name = malloc(name_len + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	strcpy(new_dog->name, name); /* copy the name */
 
-	/* set age */
-	new_dog->age = age;
-
-	return (new_dog);
 
 	/* allocation and copy the owner */
-	new_dog->owner = strdup(owner);
+	new_dog->owner = malloc(owner_len + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
@@ -44,5 +44,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	strcpy(new_dog->owner, owner); /*copy the owner */
+
+
+	/* set age */
+	new_dog->age = age;
+
 	return (new_dog);
+
 }
