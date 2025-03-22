f6d531c7c88dc203f23bf85b67e08138f03648ff
M0simi <mish5508@gmail.com>
2025-03-22 21:20:07 +0000

    betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,17 +3,20 @@
 #include "variadic_functions.h"
 
 /**
+ * print_numbers - Prints numbers separated by a given string.
+ * @separator: The string used to separate numbers.
+ * @n: The number of integers to print.
  *
- *
+ * Return: Nothing (void function).
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list args; /* correction the wrong: defination lis parameters */
 	unsigned int i;
 
-	va_start(args, n);
+	va_start(args, n); /* start reading */
 
-	for (i = 0; i < n;i++)
+	for (i = 0; i < n ; i++)
 	{
 		printf("%d", va_arg(args, int)); /* print currect number */
 
