565eea78386be7297252c7bcb10bb2ceee2dfddb
M0simi <mish5508@gmail.com>
2025-04-06 03:54:07 +0000

    edit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(from_fd);
 		close(to_fd);
-		exit(98);
+		exit(99);
 	}
 
 	check_close(from_fd);
